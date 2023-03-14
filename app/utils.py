0ad44bb14dff47748b75d5ebafc8c8b6d6207405
jitesh <jiteshchourasia48@gmail.comgit config --global user.name jiteshgit config --global user.email jiteshchourasia48@gmail.com>
2023-03-14 20:49:30 +0530

    commited

---
+++ app/utils.py
@@ -0,0 +1,49 @@
+import pickle
+import json
+import numpy as np
+
+class prediction():
+    def __init__(self):
+        pass
+
+    def load_raw(self):
+        with open("artifacts\concrete.pkl","rb") as model_file:
+            self.model=pickle.load(model_file)
+
+        with open("artifacts\column_names.json","r") as col_file:
+            self.column_names=json.load(col_file)    
+
+    def predict_strength(self,data):
+        self.load_raw()
+        self.data=data
+
+        user_input=np.zeros(len(self.column_names["Column Names"]))
+
+        Cement=data["html_cement"]
+        Blast_Furnace_Slag=data["html_bfs"]
+        Fly_Ash=data["html_fly"]
+        Water=data["html_water"]
+        Superplasticizer=data["html_sp"]
+        Coarse_Aggregate=data["html_ca"]
+        Fine_Aggregate=data["html_fa"]
+        Age=data["html_age"]
+
+        user_input[0]=Cement
+        user_input[1]=Blast_Furnace_Slag
+        user_input[2]=Fly_Ash
+        user_input[3]=Water
+        user_input[4]=Superplasticizer
+        user_input[5]=Coarse_Aggregate
+        user_input[6]=Fine_Aggregate
+        user_input[7]=Age
+
+        print(f"{user_input=}")
+        compressive_strength=self.model.predict([user_input])
+
+        return (f"Predicted Compressive Strength in Mpa = {compressive_strength}")
+    
+if __name__ == "__main__":
+    pred_obj=prediction()
+    pred_obj.load_raw
+
+        
---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,request
+from app.utils import prediction
+
+app=Flask(__name__)
+
+@app.route("/csc",methods=["POST","GET"])
+def start():
+    return render_template("concrete.html")
+
+@app.route("/strength",methods=["POST","GET"])
+def strength():
+    data=request.form
+    pred_obj=prediction()
+    predicted_cs=pred_obj.predict_strength(data)
+    print(predicted_cs)
+
+    return(predicted_cs)
+
+if __name__=="__main__":
+    app.run( host="0.0.0.0", port=8000, debug=False)
+    
+
